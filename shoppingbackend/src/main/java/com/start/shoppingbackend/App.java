73207e74e3d8282a539195dae32fe9565b5505d0
Sindhuja Talapati <sindhuja.talapati@gmail.com>
2018-07-18 12:37:49 +0530

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/start/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.start.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
